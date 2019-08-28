60d7e8f50c382e121c3e91137e4317ad908fb4ed
Sam Goodger <sam@dcode.com.au>
2019-08-28 15:45:21 +1000

    random stuff

--- src/views/Wiki.vue
+++ src/views/Wiki.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="wiki">
-    <h1>Wiki</h1>
-    <component :is="html"></component>
+    <aside>
+      <sidebar />
+    </aside>
+    <article>
+      <component :is="html"></component>
+    </article>
   </div>
 </template>
 
 <script>
   export default {
     components: {
-      // 'wiki-page': resolve => require(['@/wiki/' + this.$route.params.page + '.md'], resolve),
+      'sidebar': resolve => require(['@/wiki/_Sidebar.md'], resolve),
     },
     data() {
       return {
@@ -18,6 +22,32 @@
     created() {
       const page = this.$route.params.page;
       this.html = require('@/wiki/'+page+'.md').default;
+    },
+    mounted() {
+      document.addEventListener('click', event => {
+        event.preventDefault();
+        console.log(event);
+      })
     }
   }
 </script>
+
+<style lang="scss">
+  .wiki {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  aside {
+    flex: 0 0 16em;
+    position: sticky;
+    top: 0;
+    background: #EEE;
+    padding: 2em;
+  }
+
+  article {
+    padding: 2em;
+    max-width: 800px;
+  }
+</style>
