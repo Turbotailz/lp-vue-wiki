8a07b5f6022e3c69323aae39d4a4274009d63f8a
Sam Goodger <sam@dcode.com.au>
2019-08-28 14:58:12 +1000

    init

---
+++ src/views/Wiki.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wiki">
+    <h1>Wiki</h1>
+    <component :is="html"></component>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      // 'wiki-page': resolve => require(['@/wiki/' + this.$route.params.page + '.md'], resolve),
+    },
+    data() {
+      return {
+        html: null,
+      }
+    },
+    created() {
+      const page = this.$route.params.page;
+      this.html = require('@/wiki/'+page+'.md').default;
+    }
+  }
+</script>
